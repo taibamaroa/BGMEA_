82cd81a89896553cf8b3c1915b740712893eaf6d
Taibaelias <maroaelies@gmail.com>
2026-04-10 01:35:27 +0600

    create login

--- src/main/java/com/example/bgmea/bgmea_/HelloApplication.java
+++ src/main/java/com/example/bgmea/bgmea_/HelloApplication.java
@@ -10,10 +10,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/example/bgmea/bgmea_/Login.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("BGMEA ");
         stage.setScene(scene);
         stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
     }
 }
---
+++ src/main/java/com/example/bgmea/bgmea_/Login.java
@@ -0,0 +1,51 @@
+package com.example.bgmea.bgmea_;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import java.io.IOException;
+
+public class Login {
+    @javafx.fxml.FXML
+    private PasswordField passwordTextField;
+    @javafx.fxml.FXML
+    private Label errorLabel;
+    @javafx.fxml.FXML
+    private TextField usernameTextField;
+    //taiba
+    private final String FinanceID = "Finance";
+    private final String Financepassword = "1234";
+    private final String HRID = "HR";
+    private final String HRpassword = "1234";
+
+    @FXML
+    public void LoginOnActionButton(ActionEvent actionEvent) throws IOException {
+        String username = usernameTextField.getText().trim();
+        String password = passwordTextField.getText().trim();
+// Taiba
+        if (username.equals(FinanceID) && password.equals(Financepassword)) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/bgmea/Taiba/FinanceDashboard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) usernameTextField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        } else if (username.equals(HRID) && password.equals(HRpassword)) {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/example/bgmea/Taiba/HRDashboard.fxml"));
+            Parent root = loader.load();
+            Stage stage = (Stage) usernameTextField.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+
+
+        } else {
+            errorLabel.setText("Incorrect username or password. Please try again.");
+        }
+    }
+
+}
